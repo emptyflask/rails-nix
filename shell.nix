dcf15ef7f4f82f4a1b52ca9b187cc6bfb02597d9
Jon Roberts <jon@emptyflask.net>
2020-08-17 12:13:53 -0500

    no good reason to make shell.nix a symlink
    also removed `config` since it's not really useful here

--- shell.nix
+++ shell.nix
@@ -1,1 +1,21 @@
-nix/shell.nix
+let
+  sources  = import ./nix/sources.nix;
+  pkgs     = import sources.nixpkgs  {};
+  unstable = import sources.unstable {};
+  rubyenv  = pkgs.callPackage ./nix/rubyenv.nix {};
+
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      bundix
+      bundler
+      bundler-audit
+      libxml2
+      rubyenv
+      rubyenv.wrappedRuby
+      sassc
+      yarn
+
+      unstable.nodejs-12_x
+    ];
+  }
