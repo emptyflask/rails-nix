1c01042ce09f1299680d8f2028b477a7f5c10453
Jon Roberts <jon@emptyflask.net>
2021-03-05 10:08:37 -0600

    update sources, rename shell to default

--- default.nix
+++ default.nix
@@ -5,17 +5,30 @@ let
   rubyenv  = pkgs.callPackage ./nix/rubyenv.nix {};
 
 in
-  pkgs.mkShell {
+  pkgs.stdenv.mkDerivation rec {
+    name = "rails6-demo";
+    src = ./.;
+
+    env = rubyenv;
+
     buildInputs = with pkgs; [
       bundix
       bundler
       bundler-audit
       libxml2
+      makeWrapper
       rubyenv
       rubyenv.wrappedRuby
       sassc
       yarn
-
       unstable.nodejs-12_x
     ];
+
+    # phases = ["installPhase"];
+
+    installPhase = ''
+      mkdir -p $out
+      cp -a . $out
+    '';
   }
+
